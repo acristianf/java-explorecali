eeae5b3dad2fc7df4ac78c2ca91830f59f9a8de9
acristianf <acristianf@hotmail.com>
2023-07-18 22:36:33 -0300

    Added logging.

--- src/main/java/com/cristian/ec/explorecali/services/TourRatingService.java
+++ src/main/java/com/cristian/ec/explorecali/services/TourRatingService.java
@@ -3,23 +3,31 @@ package com.cristian.ec.explorecali.services;
 import com.cristian.ec.explorecali.domain.TourRating;
 import com.cristian.ec.explorecali.repo.TourRatingRepository;
 import com.cristian.ec.explorecali.repo.TourRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Arrays;
 
 @Service
 @Transactional
 public class TourRatingService {
-    TourRatingRepository tourRatingRepository;
-    TourRepository tourRepository;
+
+    private final Logger LOGGER = LoggerFactory.getLogger(TourRatingService.class);
+    private TourRatingRepository tourRatingRepository;
+    private TourRepository tourRepository;
 
     TourRatingService(TourRatingRepository tourRatingRepository, TourRepository tourRepository) {
         this.tourRatingRepository = tourRatingRepository;
         this.tourRepository = tourRepository;
     }
 
-    public void createManyTourRatings(Integer tourId, Integer score, Integer[] customers) {
+    public void rateMany(Integer tourId, Integer score, Integer[] customers) {
+        LOGGER.info("Rate tour {} by customers {}", tourId, Arrays.asList(customers));
         tourRepository.findById(tourId).ifPresent(tour -> {
             for (Integer customer : customers) {
+                LOGGER.debug("Attempt to create rating by customer {}", customer);
                 tourRatingRepository.save(new TourRating(tour, customer, score));
             }
         });
--- src/main/java/com/cristian/ec/explorecali/web/TourRatingController.java
+++ src/main/java/com/cristian/ec/explorecali/web/TourRatingController.java
@@ -1,13 +1,13 @@
 package com.cristian.ec.explorecali.web;
 
 import com.cristian.ec.explorecali.domain.Tour;
-import com.cristian.ec.explorecali.domain.TourPackage;
 import com.cristian.ec.explorecali.domain.TourRating;
 import com.cristian.ec.explorecali.domain.TourRatingPk;
 import com.cristian.ec.explorecali.repo.TourRatingRepository;
 import com.cristian.ec.explorecali.repo.TourRepository;
 import com.cristian.ec.explorecali.services.TourRatingService;
-import jakarta.transaction.Transactional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
@@ -16,18 +16,17 @@ import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.NoSuchElementException;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(path = "/tours/{tourId}/ratings")
 public class TourRatingController {
-    TourRatingRepository tourRatingRepository;
-    TourRepository tourRepository;
-    TourRatingService tourRatingService;
+    private static final Logger LOGGER = LoggerFactory.getLogger(TourRatingController.class);
+    private TourRatingRepository tourRatingRepository;
+    private TourRepository tourRepository;
+    private TourRatingService tourRatingService;
 
     @Autowired
     public TourRatingController(TourRatingRepository tourRatingRepository, TourRepository tourRepository, TourRatingService tourRatingService) {
@@ -42,6 +41,7 @@ public class TourRatingController {
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public void createTourRating(@PathVariable(value = "tourId") int tourId, @RequestBody @Validated RatingDto ratingDto) {
+        LOGGER.info("POST /tours/{}/ratings", tourId);
         Tour tour = verifyTour(tourId);
         tourRatingRepository
                 .save(
@@ -54,6 +54,7 @@ public class TourRatingController {
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public Page<RatingDto> getTourRatings(@PathVariable(value = "tourId") int tourId, Pageable pageable) {
+        LOGGER.info("GET /tours/{}/ratings", tourId);
         verifyTour(tourId);
         Page<TourRating> ratings = tourRatingRepository.findByPkTourId(tourId, pageable);
         List<RatingDto> ratingDtos = ratings.stream().map(rating -> new RatingDto(rating.getRating(), rating.getComment(), rating.getPk().getCustomerId())).toList();
@@ -63,6 +64,7 @@ public class TourRatingController {
     @GetMapping(path = "/average")
     @ResponseStatus(HttpStatus.OK)
     public Map<String, Double> getAverage(@PathVariable(value = "tourId") int tourId) {
+        LOGGER.info("GET /tours/{}/ratings/average", tourId);
         verifyTour(tourId);
         return Map.of(
                 "average",
@@ -77,6 +79,7 @@ public class TourRatingController {
     @PutMapping
     @ResponseStatus(HttpStatus.OK)
     public RatingDto updateRating(@PathVariable(value = "tourId") int tourId, @RequestBody @Validated RatingDto ratingDto) {
+        LOGGER.info("PUT /tours/{}/ratings", tourId);
         TourRating tourRating = verifyTourRating(tourId, ratingDto.getCustomerId());
         tourRating.setRating(ratingDto.getScore());
         tourRating.setComment(ratingDto.getComment());
@@ -86,6 +89,7 @@ public class TourRatingController {
     @PatchMapping
     @ResponseStatus(HttpStatus.OK)
     public RatingDto updateRatingWithPatch(@PathVariable(value = "tourId") int tourId, @RequestBody @Validated RatingDto ratingDto) {
+        LOGGER.info("PATCH /tours/{}/ratings", tourId);
         TourRating tourRating = verifyTourRating(tourId, ratingDto.getCustomerId());
         if (ratingDto.getScore() != null) {
             tourRating.setRating(ratingDto.getScore());
@@ -99,6 +103,7 @@ public class TourRatingController {
     @DeleteMapping(path = "/{customerId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteRating(@PathVariable(value = "tourId") int tourId, @PathVariable(value = "customerId") int customerId) {
+        LOGGER.info("DELETE /tours/{}/ratings/{}", tourId, customerId);
         TourRating tourRating = verifyTourRating(tourId, customerId);
         tourRatingRepository.delete(tourRating);
     }
@@ -108,7 +113,8 @@ public class TourRatingController {
     public void createManyTourRatings(@PathVariable(value = "tourId") int tourId,
                                       @PathVariable(value = "score") int score,
                                       @RequestParam("customers") Integer[] customers) {
-        tourRatingService.createManyTourRatings(tourId, score, customers);
+        LOGGER.info("POST /tours/{}/ratings/{}", tourId, customers);
+        tourRatingService.rateMany(tourId, score, customers);
     }
 
     private Tour verifyTour(int tourId) throws NoSuchElementException {
@@ -126,6 +132,7 @@ public class TourRatingController {
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(NoSuchElementException.class)
     public String return400(NoSuchElementException ex) {
+        LOGGER.error("Unable to complete transaction", ex);
         return ex.getMessage();
     }
 }
